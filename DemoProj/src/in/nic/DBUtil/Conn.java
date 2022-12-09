de69cddbbd9b6e4c758db4bdbc7a3bb7d48d24e7
Ramsunil Samsani <ramsunilsamsani55@gmail.com>
2022-12-09 12:47:06 +0530

    Added con and modified bean A.java by ramsunil

---
+++ DemoProj/src/in/nic/DBUtil/Conn.java
@@ -0,0 +1,5 @@
+package in.nic.DBUtil;
+
+public class Conn {
+
+}
--- DemoProj/src/in/nic/beans/A.java
+++ DemoProj/src/in/nic/beans/A.java
@@ -1,5 +1,37 @@
 package in.nic.beans;
 
 public class A {
+ private String a;
+ private int b;
 
+ public A(String a, int b) {
+	super();
+	this.a = a;
+	this.b = b;
+}
+
+public String getA() {
+	return a;
+}
+
+public void setA(String a) {
+	this.a = a;
+}
+
+public int getB() {
+	return b;
+}
+
+public void setB(int b) {
+	this.b = b;
+}
+
+@Override
+public String toString() {
+	return "A [a=" + a + ", b=" + b + "]";
+}
+ 
+ 
+ 
+ 
 }
